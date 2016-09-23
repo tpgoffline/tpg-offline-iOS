99a58d8503005462c1da75f1ea4eaa11c5113e1d
Da Costa Faro Rémy <remy@dacostafaro.com>
2016-09-23 15:37:03 +0200

    Fix Travis CI

--- travisci.sh
+++ travisci.sh
@@ -2,4 +2,4 @@
 
 set -e
 
-xcodebuild -workspace "iOS/tpg offline.xcodeproj" -scheme "tpg offline Travis CI" -destination "platform=iOS Simulator,name=iPhone 7" test | xcpretty -c
+xcodebuild -project "iOS/tpg offline.xcodeproj" -scheme "tpg offline Travis CI" -destination "platform=iOS Simulator,name=iPhone 7" test | xcpretty -c
