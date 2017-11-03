ecef0f863f74ab66d8d106ca5c45bc2b90fb942c
Remy Da Costa Faro <remy@dacostafaro.com>
2017-11-03 11:58:32 +0100

    Add API.swift

---
+++ tpg offline/Project Requirements/API.swift
@@ -0,0 +1,13 @@
+//
+//  API.swift
+//  tpg offline
+//
+//  Created by Rémy DA COSTA FARO on 03/11/2017.
+//  Copyright © 2017 Remy. All rights reserved.
+//
+
+import Foundation
+
+struct API {
+    let key = "" // Put your tpg open data API key here
+}
