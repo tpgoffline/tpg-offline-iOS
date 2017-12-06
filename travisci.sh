2751fe748c772cb018cf6227674e0aefc85a4145
Remy Da Costa Faro <remy@dacostafaro.com>
2017-12-06 15:23:37 +0100

    Test #1

--- travisci.sh
+++ travisci.sh
@@ -1,2 +1,10 @@
 set -e
+cd JSON
+if [[ $(md5 -q stops.json | tr -d '\n' | sort | diff stops.json.md5 -) ]] then
+    echo "ERROR: stops.json.md5 is incorrect"
+fi
+if [[ $(md5 -q departures.json | tr -d '\n' | sort | diff departures.json.md5 -) ]] then
+    echo "ERROR: departures.json.md5 is incorrect"
+fi
+cd ..
 xcodebuild -project "tpg offline.xcodeproj" -scheme "tpg offline" -destination "platform=iOS Simulator,name=iPhone 8" test | xcpretty -c
