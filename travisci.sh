65c3be23898bcece7b9117a694027f60e4e2b98c
Remy Da Costa Faro <remy@dacostafaro.com>
2018-01-20 08:05:00 +0100

    Updated departures

--- travisci.sh
+++ travisci.sh
@@ -2,13 +2,15 @@ set -e
 cd JSON
 if [[ $(md5 -q stops.json | tr -d '\n' | sort | diff stops.json.md5 -) ]] 
 then
-    echo "ERROR: stops.json.md5 is incorrect"
+    echo "✘ stops.json.md5 is incorrect"
     (exit -1)
+else
+    echo "✔︎ stops.json.md5 is correct"
 fi
 if [[ $(md5 -q departures.json | tr -d '\n' | sort | diff departures.json.md5 -) ]] 
 then
-    echo "ERROR: departures.json.md5 is incorrect"
+    echo "✘ departures.json.md5 is incorrect"
     (exit -1)
+else
+    echo "✔︎ departures.json.md5 is correct"
 fi
-cd ..
-xcodebuild -project "tpg offline.xcodeproj" -scheme "tpg offline" -destination "platform=iOS Simulator,name=iPhone 8" test | xcpretty -c
